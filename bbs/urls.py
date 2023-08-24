5fcba4108579c70b76b3936f09aac3e7038f5ec1
wwwxl <511969341@qq.com>
2023-08-24 23:18:59 +0800

    modified:   bbs/__pycache__/models.cpython-37.pyc 	modified:   bbs/__pycache__/urls.cpython-37.pyc 	modified:   bbs/__pycache__/views.cpython-37.pyc 	modified:   bbs/templates/bbs/detail.html 	modified:   bbs/templates/bbs/index.html 	new file:   bbs/templates/bbs/login_page.html 	new file:   bbs/templates/bbs/register.html 	modified:   bbs/urls.py 	modified:   bbs/views.py 	modified:   db.sqlite3

--- bbs/urls.py
+++ bbs/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("subart", views.subart, name="subart"),
+    path('login_page',views.login_page,name='login_page'),
+    path('register',views.register,name='register'),
+    path('logout_view',views.logout_view,name = 'logout'),
+    path('<int:article_id>/subcomment',views.subcomment,name='subcomment'),
+
 ]
--- bbs/views.py
+++ bbs/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import generic
 from django.utils import timezone
 from .models import Article,Comment
 from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+
+
+
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = "bbs/index.html"
@@ -10,7 +17,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published comment."""
-        return Article.objects.order_by("pub_date")[:5]
+        return Article.objects.order_by("-pub_date")
     
 class DetailView(generic.DetailView):
     model = Article
@@ -21,54 +28,103 @@ class DetailView(generic.DetailView):
 
 def subart(request):
     if request.method == "POST":
-        print(request.POST.getlist())
-
-    #     # 获得表单
-    #     article_post_form = ArticlePostForm(data=request.POST)
-    #     if article_post_form.is_valid():
-    #         cd = article_post_form.cleaned_data
-    #         try:
-    #             # 将表单数据放入到article_post_form关联的模型ArticlePost
-    #             new_article = article_post_form.save(commit=False)
-    #             # 文章关联的作者
-    #             new_article.author = request.user
-    #             # 文章关联的类型
-    #             new_article.column = request.user.article_column.get(id=request.POST['column_id'])
-    #             # 保存到数据库
-    #             new_article.save()
-    #             return HttpResponse("1")
-    #         except:
-    #             return HttpResponse("2")
-    #     else:
-    #         return HttpResponse("3")
-    # else:
-    #     article_post_form = ArticlePostForm()
-    #     # 查得该用户的所有栏目
-    #     article_columns = request.user.article_column.all()
-    #     # 入参包括表单和栏目
-    #     return render(request, "article/column/article_post.html",
-    #                   {"article_post_form": article_post_form, "article_columns": article_columns,})
+        t = request.POST.get("title")
+        b = request.POST.get("body")
+        if request.user.is_authenticated:
+            if t and b:
+                Article.objects.create(Article_title = t,Article_text=b,pub_date=timezone.now(),author=request.user)
+        
+                return HttpResponseRedirect(reverse('bbs:index'))   # app名：url名
+        else:
+            return HttpResponseRedirect(reverse('bbs:login_page'))
+        # return HttpResponseRedirect('/bbs/')
 
 
-# def vote(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     try:
-#         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(
-#             request,
-#             "polls/detail.html",
-#             {
-#                 "question": question,
-#                 "error_message": "You didn't select a choice.",
-#             },
-#         )
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
 
-#     return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+def register(request):
+    if request.method == 'GET':
+        return render(request,'bbs/register.html')
+    if request.method == "POST":
+        print(request.user.is_authenticated)
+        u = request.POST.get("username")
+        p = request.POST.get("password")
+        print(u,p)
+        if u and p:
+            if not User.objects.filter(username = u).exists():
+                new_user = User.objects.create_user(username=u,password=p)
+                print(new_user)
+                new_user.save()
+                return HttpResponseRedirect(reverse('bbs:index'))   # app名：url名
+            else:
+                return render(
+                request,
+                "bbs/register.html",
+                {
+                    "error_message": "注册失败了老铁！用户名已经存在！",
+                },)
+
+        else:
+            return render(
+                request,
+                "bbs/register.html",
+                {
+                    "error_message": "你好像什么都没填啊老铁",
+                },)
+
+
+
+
+def login_page(request):
+    if request.method == 'GET':    
+        print(request.user.is_authenticated)
+        if request.user.is_authenticated:
+            return HttpResponse("你已经登陆了！")
+        else:
+            return render(request,'bbs/login_page.html')    
+
+    if request.method == "POST":
+        print(request.user.is_authenticated)
+        u = request.POST.get("username")
+        p = request.POST.get("password") 
+        print(u,p)
+        if u and p:
+            user = authenticate(username=u,password=p)
+            if user is not None:
+                login(request,user)
+                print(request.user.is_authenticated)
+                return HttpResponseRedirect(reverse('bbs:index'))
+                
+
+            else:
+                return render(
+                request,
+                "bbs/login_page.html",
+                {
+                    "error_message": "登录失败了老铁！用户名或者密码不对呀！",
+                },)
+
+        else:
+            return render(
+                request,
+                "bbs/login_page.html",
+                {
+                    "error_message": "你好像什么都没填啊老铁",
+                },)
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('bbs:index'))
+
+
+def subcomment(request,article_id):
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            b = request.POST.get('comment')
+            art = get_object_or_404(Article, pk=article_id)
+            print(art)
+            Comment.objects.create(article = art,comment_text=b,pub_date=timezone.now(),author=request.user)        
+            return HttpResponseRedirect(reverse("bbs:detail", args=(article_id,)))
+        else:
+            return HttpResponseRedirect(reverse('bbs:login_page'))
+   
+
